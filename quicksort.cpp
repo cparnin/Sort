fdd166aace377810f43687b535bd7236608e9ba3
Chad Parnin <cparnin@users.noreply.github.com>
2014-11-16 19:04:19 -0500

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,144 +1,120 @@
-/*
+/*************************************************
  
 Name:  Chad Parnin
-Date:  11/12/14.
+Date:  11/13/14
 File:  quicksort.cpp
-Description:
+Description:  Quicksort implementation on array
  
-*/
+*************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <pthread.h>
-// not using namespace to speed up a bit (?)
+//#include <pthread.h>
+#include <sys/time.h>
+#include <ctime> // REMOVE BEFORE TURNING IN
 
-int partition ( int *p[], int first, int last );
-void swap ( int *a, int *b );
-void quicksort ( int *p[], int first, int last );
-
-//int is_sorted ( int a[], int n )
-//{
-//    int i;
-//    for ( i = 0; i < n - 1; i++ )
-//    {
-//        if ( a[i] > a[i + 1] )
-//            return 0;
-//    }
-//    return 1;
-//}
-
+//int partition ( int p[], int first, int last ); // Median of Three partition (slower)
+void swap ( int &a, int &b );
+void quicksort ( int p[], int first, int last );
 
 int main()
 {
-    std::ifstream infile;
-    infile.open("test500.txt");
-    
-    if (infile.good())
-    {
-        int *p;
-        int sizeOfArray;
-        std::cout << "Size of array?\n";
-        std::cin >> sizeOfArray;
-        p = new int[sizeOfArray];
+    srand(time(NULL)); // seed RNG - REMOVE BEFORE TURNING IN
 
-        for (int i = 0; i < sizeOfArray; ++i)
-        {
-            infile.get() >> p[i];
-        }
-        
-        
-        
-        delete [] p;
-        infile.close();
-        
-    }
-    else
-        std::cout << "Error Reading File/n";
+    struct timeval before, after; // timing variables
+    double timing;
+    
+    std::cout << "Size of array?\n";
+    int sizeOfArray;
+    std::cin >> sizeOfArray;
+    int *p = new int[sizeOfArray]; // create dynamic array
+    for (int i = 0; i < sizeOfArray; ++i)
+        //std::cin >> p[i]; // used for cin redirection (Linux = ./a.out < test.txt)
+        p[i] = rand() % 100; // randomize the array - REMOVE BEFORE TURNING IN
+    
+    gettimeofday(&before, 0); // start timing
+    quicksort(p, 0, sizeOfArray-1); // sort it
+    gettimeofday(&after, 0); // end timing
+    
+    timing = (double) ((double)after.tv_sec +
+                      (double)after.tv_usec/(1000*1000)) -
+                      (double) ((double)before.tv_sec +
+                      (double)before.tv_usec/(1000*1000));
+    
+    //for (int i = 0; i < sizeOfArray; ++i) // output array
+      //  std::cout << p[i] << std::endl;
+    
+    std::cout << "Quicksort took " << timing << "seconds\n"; // output timing information
+ 
+    delete [] p; // delete dynamic array
     
     return 0;
 }
 
-int partition ( int *p[], int first, int last )
+void swap ( int &a, int &b )
 {
-    int pivot, mid = ( first + last ) / 2;
-    /* Largest of two */
-    pivot = p[first] > p[mid] ? first : mid;
-    
-    /* Smallest of remaining */
-    if ( p[pivot] > p[last] )
-        pivot = last;
-    swap ( p[pivot], p[first] );
-    pivot = first;
-    while ( first < last )
+    int c = a;
+    a = b;
+    b = c;
+}
+
+void quicksort(int p[], int first, int last)
+{
+    if (last <= first)
+        return;
+    //int median = (p[first] + p[last] + p[(first+last)/2]) / 3;
+    //swap(p[first],median);
+    int pivot = p[first];
+    int i=first+1, j=last;
+    while (i<j)
     {
-        if ( p[first] <= p[last] )
-            swap ( p[pivot++], p[first] );
-        ++first;
+        while (p[i] < pivot && i<j)
+            i++;
+        while (p[j] > pivot)
+            j--;
+        if (i<j)
+            swap(p[i++], p[j--]);
     }
-    swap ( p[pivot], p[last] );
-    return pivot;
-}
-
-void swap ( int *a, int *b )
-{
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
-
-void quicksort ( int *p[], int first, int last )
-{
-    if ( first < last )
-    {
-        int pivot = partition ( p, first, last );
-        quicksort ( p, first, pivot - 1 );
-        quicksort ( p, pivot + 1, last );
-    }
-}
+    if (p[j] > pivot)
+        j--;
+    swap(p[j], p[first]);
+    quicksort(p, first, j-1);
+    quicksort(p, j+1, last);
+} // quicksort()
 
 
-/*
+//int partition ( int p[], int first, int last )
+//{
+//    int pivot, mid = ( first + last ) / 2;
+//    /* Largest of two */
+//    pivot = p[first] > p[mid] ? first : mid;
+//    
+//    /* Smallest of remaining */
+//    if ( p[pivot] > p[last] )
+//        pivot = last;
+//    swap ( p[pivot], p[first] );
+//    pivot = first;
+//    while ( first < last )
+//    {
+//        if ( p[first] <= p[last] )
+//            swap ( p[pivot++], p[first] );
+//        ++first;
+//    }
+//    swap ( p[pivot], p[last] );
+//    return pivot;
+//}
 
- #include <iostream>
- 2 #include <time.h>
- 3 #include <sys/time.h>
- 4 using namespace std;
- 5
- 6 void test_1(int x[]);
- 7 void test_2(int y[]);
- 8
- 9 int main()
- 10 {
- 11     struct timeval b,e;
- 12     gettimeofday (&b, NULL);
- 13
- 14     int num[2000000] = {0};
- 15     for (int i = 0; i < 2000000; i++)
- 16     num[i] = i+1;
- 17
- 18     test_1(num);
- 19     test_2(num);
- 20
- 21     gettimeofday (&e, NULL);
- 22     cout << "It took " << ( ( e.tv_sec - b.tv_sec ) + ( e.tv_usec - b.tv_us    ec ) * 1e-6) << " seconds." << endl << endl;
- 23
- 24     return 0;
- 25 }
- 26
- 27 void test_1(int x[])
- 28 {
- 29     int sum1 = 0;
- 30     for (int i = 0; i < 1000000; i++)
- 31     sum1 += x[i];
- 32     cout << sum1 << endl;
- 33 }
- 34
- 35 void test_2(int y[])
- 36 {
- 37     int sum2 = 0;
- 38     for (int i = 1000000; i < 2000000; i++)
- 39     sum2 += y[i];
- 40     cout << sum2 << endl;
- 41 }
- 
- */
+//void quicksort ( int p[], int first, int last )
+//{
+//    if ( first < last )
+//    {
+//        int pivot = partition ( p, first, last );
+//        quicksort ( p, first, pivot - 1 );
+//        quicksort ( p, pivot + 1, last );
+//    }
+//}
+
+
+
+
+
+
